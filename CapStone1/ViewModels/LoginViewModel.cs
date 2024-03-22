6c99b63de11dff32d89f156310798eb6585c3e70
yundonghee <dong000424@naver.com>
2024-03-23 00:03:27 +0900

    프론트엔드 기본틀 완성

---
+++ CapStone1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿// LoginViewModel.cs
+namespace CapStone1.ViewModels
+{
+    // LoginViewModel.cs
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
