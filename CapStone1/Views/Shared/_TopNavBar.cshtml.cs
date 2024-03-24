30435f1fd064622f6ee0a840f6419602a8e57dde
yundonghee <dong000424@naver.com>
2024-03-24 21:22:24 +0900

    기본틀 완성, 로그인과 회원가입 기능빼고 기본틀 완성, 네비바 모양 틀 완성

---
+++ CapStone1/Views/Shared/_TopNavBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CapStone1.Views.Shared
+{
+    public class _TopNavBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
